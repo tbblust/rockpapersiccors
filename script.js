c06201f8d33ded8948881e71dee6743a6c13b963
Tyler Blust <tbblustllc@gmail.com>
2022-10-25 00:08:07 -0400

    Adds playround functionality. off script from OTP

--- script.js
+++ script.js
@@ -16,3 +16,22 @@ function getComputerChoice()
         return "Scissors";
     }
 }
+
+function playRound(playerSelection, computerSelection)
+{
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+    
+    if(playerSelection === computerSelection)
+    {
+        return playerSelection + " against " + computerSelection + ". Tie Game."
+    }
+    else if ((playerSelection === "Rock" && computerSelection === "Paper") || (playerSelection === "Paper" && computerSelection === "Scissors") || (playerSelection === "Scissors" && computerSelection === "Rock"))
+    {
+        return "You Lose! " + computerSelection + " beats " + playerSelection + " .";
+    }
+    else
+    {
+        return "You Win! " + playerSelection + " beats " + computerSelection + " .";
+    }
+
+}
